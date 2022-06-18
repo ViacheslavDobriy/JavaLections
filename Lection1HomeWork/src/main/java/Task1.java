9adc3a10c629ba0bef2027d2e943c7ae94cae706
Vyacheslav <dobrov.slava@yandex.ru>
2022-06-18 21:43:07 +0300

    Added method which generate numbers from 0 to 100 - it will be ratios of our polynomial. Also imported instances Arrays and Random. Need to fix next: 1) There is only positive numbers from random; 2) Check user's number. It should be more or equal 2.

--- Lection1HomeWork/src/main/java/Task1.java
+++ Lection1HomeWork/src/main/java/Task1.java
@@ -4,7 +4,9 @@
  *  Пример: k = 2 => 2*x^2 + 4*x + 5 = 0 или x^2 + 5 = 0 или 10*x^2 = 0
  */
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Task1 {
 
@@ -14,7 +16,21 @@ public class Task1 {
         return iScanner.nextInt();
     }
 
+    static int[] CreatingArrayOfRatios(int UserNumber){
+        int[] ArrayOfRatios = new int[UserNumber+1];
+        int count = 0;
+        Random rnd = new Random(System.currentTimeMillis());
+        while (UserNumber>=0){
+            ArrayOfRatios[count] = rnd.nextInt(101);
+            count++;
+            UserNumber--;
+        }
+        return ArrayOfRatios;
+    }
+
     public static void main(String[] args) {
-        System.out.println(GettingNumber());
+        int userNumber = GettingNumber();
+        System.out.printf("%d\n", userNumber);
+        System.out.println(Arrays.toString(CreatingArrayOfRatios(userNumber)));
     }
 }
