65ad6512efc06826917b22f84f362cd118c9c096
Vyacheslav <dobrov.slava@yandex.ru>
2022-06-18 21:11:44 +0300

    Added method GettingNumber. User should insert integer number. Main is working well, without error.

--- Lection1HomeWork/src/main/java/Task1.java
+++ Lection1HomeWork/src/main/java/Task1.java
@@ -4,9 +4,18 @@
  *  Пример: k = 2 => 2*x^2 + 4*x + 5 = 0 или x^2 + 5 = 0 или 10*x^2 = 0
  */
 
+import java.util.Scanner;
+
 public class Task1 {
 
-    public static void main(String[] args) {
+    static int GettingNumber(){
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Insert ratio of the polynomial: ");
+        int userNumber = iScanner.nextInt();
+        return userNumber;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(GettingNumber());
     }
 }
