bc9751395144e5bcea28775c59428492a164329d
ViacheslavDobriy <dobrov.slava@yandex.ru>
2022-06-20 14:09:28 +0800

    Added method isCorrect for checking inserted string.

--- Seminar1HomeWork/src/main/java/Task1.java
+++ Seminar1HomeWork/src/main/java/Task1.java
@@ -5,15 +5,28 @@
 import java.util.Scanner;
 
 public class Task1 {
+
     static String GetNumber(){
         Scanner iScanner = new Scanner(System.in);
         System.out.println("Insert integer number - i will make triangular number!");
-        String userInsert = iScanner.nextLine();
-        iScanner.close();
-        return userInsert;
+        return iScanner.nextLine();
+    }
+
+    static boolean isCorrect(String line){
+        try {
+            if(Integer.parseInt(line)>0)
+                return true;
+        }
+        catch (NumberFormatException e){
+            return false;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
-
+        while(!isCorrect(GetNumber())){
+            System.out.println("This is not correct, try again!");
+        }
+            System.out.println("This is correct");
     }
 }
