f341934d7159861ff7a0c7b49c7f5c14229c240b
ViacheslavDobriy <dobrov.slava@yandex.ru>
2022-06-20 13:53:17 +0800

    Added method GetNumber() for receiving input number from user.

--- Seminar1HomeWork/src/main/java/Task1.java
+++ Seminar1HomeWork/src/main/java/Task1.java
@@ -1,7 +1,18 @@
 /**
  * Write a program for calculating the n-th triangular number
  */
+
+import java.util.Scanner;
+
 public class Task1 {
+    static String GetNumber(){
+        Scanner iScanner = new Scanner(System.in);
+        System.out.println("Insert integer number - i will make triangular number!");
+        String userInsert = iScanner.nextLine();
+        iScanner.close();
+        return userInsert;
+    }
+
     public static void main(String[] args) {
 
     }
