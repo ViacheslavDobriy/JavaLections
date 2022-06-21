74b75b3926931f7aa7f903975f5d23741c99c70f
ViacheslavDobriy <dobrov.slava@yandex.ru>
2022-06-22 02:58:06 +0800

    Create a new method SumOfDigits which count sum of digits of random number. Main is working well.

--- Seminar1HomeWork/src/main/java/Task2.java
+++ Seminar1HomeWork/src/main/java/Task2.java
@@ -7,7 +7,18 @@ public class Task2 {
         return random.nextInt(1000001);
     }
 
+    static int SumOfDigits(int randomNumber){
+        int resultSum = 0;
+        while(randomNumber!=0){
+            resultSum = resultSum + randomNumber%10;
+            randomNumber = randomNumber/10;
+        }
+        return resultSum;
+    }
+
     public static void main(String[] args) {
-        System.out.println(GettingRandomNumber());
+        int randomNumber = GettingRandomNumber();
+        System.out.println(randomNumber);
+        System.out.println(SumOfDigits(randomNumber));
     }
 }
